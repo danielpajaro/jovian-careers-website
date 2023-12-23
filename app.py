e4490bbe4c71fc053e6a065965136e46847f3551
danielpajaro <daniel.pajaro98@gmail.com>
2023-12-23 14:18:17 -0500

    segundo commit

--- app.py
+++ app.py
@@ -3,31 +3,29 @@ from markupsafe import escape
 
 app = Flask(__name__)
 
-#todos = ['APRENDER PYTHON', 'APRENDER DJANGO', 'APRENDER FLASK']
+todos = ['Learn PYTHON', 'Learn DJANGO', 'Learn FLASK']
 
-@app.route('/')
-def index():
-    return "Hello, world"
+
 
 if __name__ == "__main__":
     app.run(host='0.0.0.0', debug=True)
 
-#@app.route('/')
-#def index():
-#    user_ip = escape(request.remote_addr)
-#    response = make_response(redirect('/hello'))
-#    response.set_cookie('user_ip',user_ip)
-#
-#    return response
-#
-#
-#
-#@app.route('/hello')
-#def hello():
-#    user_ip = request.cookies.get('user_ip')
-#    context = {
-#        'user_ip': user_ip,
-#        'todos': todos
-#    }
-#    
-#    return render_template('hello.html', **context)  #'Hello Daniel, welcome to Flask Project, your IP is {}.'.format(user_ip)
+@app.route('/')
+def index():
+    user_ip = escape(request.remote_addr)
+    response = make_response(redirect('/hello'))
+    response.set_cookie('user_ip',user_ip)
+
+    return response
+
+
+
+@app.route('/hello')
+def hello():
+    user_ip = request.cookies.get('user_ip')
+    context = {
+        'user_ip': user_ip,
+        'todos': todos
+    }
+    
+    return render_template('home.html', **context)  #'Hello Daniel, welcome to Flask Project, your IP is {}.'.format(user_ip)
